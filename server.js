96b905b62224546ff0d50162a20f2cf08ba8987c
vasquezrusbel99-glitch <vasquezrusbel99@icloud.com>
2026-05-13 22:04:33 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const express = require('express');
-const app = express();
 const path = require('path');
+const app = express();
 
+// Obligamos al servidor a que use la carpeta public
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Si alguien entra a la raíz, le mandamos el index.html sí o sí
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, '0.0.0.0', () => {
-    console.log('POR FIN');
+app.listen(PORT, () => {
+    console.log('Servidor Mach Love a tope');
 });
