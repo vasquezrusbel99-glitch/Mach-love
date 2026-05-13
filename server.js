291f52ece143dc5c9e33a72bb0d9bf4334552f5f
vasquezrusbel99-glitch <vasquezrusbel99@icloud.com>
2026-05-13 21:15:37 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
+
 const express = require('express');
 const app = express();
 const path = require('path');
 
-// Esto es lo que le faltaba para encontrar la carpeta 'public' sin errores
 app.use(express.static(path.join(__dirname, 'public')));
 
 const PORT = process.env.PORT || 3000;
