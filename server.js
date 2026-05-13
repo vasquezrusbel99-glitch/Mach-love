69e048f7a88349112c5e4e76bf1af2d1227fb251
vasquezrusbel99-glitch <vasquezrusbel99@icloud.com>
2026-05-13 20:49:37 +0200

    Update server.js

--- server.js
+++ server.js
@@ -2,11 +2,11 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-// Esto sirve la carpeta public
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Render asigna el puerto automáticamente
+// ESTO ES LO QUE NECESITA RENDER:
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, '0.0.0.0', () => {
-    console.log(✅ Servidor Mach Love listo en puerto ${PORT});
+    console.log(Servidor Mach Love listo);
+});
