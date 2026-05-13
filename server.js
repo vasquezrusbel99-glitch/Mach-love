b4fc178d189a962312cb5c615e4b8ad3f5b0b146
vasquezrusbel99-glitch <vasquezrusbel99@icloud.com>
2026-05-14 00:01:13 +0200

    Update server.js

--- server.js
+++ server.js
@@ -2,13 +2,16 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+// Configura la carpeta pública para tus archivos HTML/CSS
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Ruta principal que envía el archivo index.html
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
+    
 
+// Puerto de escucha para Render
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, '0.0.0.0', () => {
-    console.log('Servidor en linea');
+    console.log('Servidor en marcha');
 });
