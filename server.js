88d7938c2ec6933ba2e22577a56242bc89c3e01c
vasquezrusbel99-glitch <vasquezrusbel99@icloud.com>
2026-05-13 21:36:18 +0200

    Update server.js

--- server.js
+++ server.js
@@ -2,9 +2,11 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
+// Esto le dice al servidor que busque tus archivos en la carpeta 'public'
 app.use(express.static(path.join(__dirname, 'public')));
 
 const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
-    console.log('Servidor en marcha');
+    console.log('Servidor Mach Love listo');
 });
