b3168951810448077897a6fb8e0610de0e95f283
vasquezrusbel99-glitch <vasquezrusbel99@icloud.com>
2026-05-13 23:27:11 +0200

    Update and rename app.js to server.js

--- server.js
+++ server.js
@@ -1,17 +1,16 @@
 const express = require('express');
 const path = require('path');
+
 const app = express();
 
-// Configuración para servir los archivos de la carpeta public
+const PORT = process.env.PORT || 3000;
+
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Ruta principal para cargar el index.html
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-// El puerto que usa Render
-const PORT = process.env.PORT || 3000;
 app.listen(PORT, '0.0.0.0', () => {
-    console.log('Servidor activo en puerto ' + PORT);
+    console.log(Servidor funcionando en puerto ${PORT});
 });
