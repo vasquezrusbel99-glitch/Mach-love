b2df1393d5ae1cb233ed73f54782bccdd8129639
vasquezrusbel99-glitch <vasquezrusbel99@icloud.com>
2026-05-13 21:13:07 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
-// AHORA SÍ: Comilla abierta y comilla cerrada
-app.use(express.static('public'));
+// Esto es lo que le faltaba para encontrar la carpeta 'public' sin errores
+app.use(express.static(path.join(__dirname, 'public')));
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log('Mach Love funcionando al 100');
+    console.log('POR FIN FUNCIONA');
 });
