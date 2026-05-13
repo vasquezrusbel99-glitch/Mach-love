68af43a1c2f20e0a6c53627622b683fc4aabe9b9
vasquezrusbel99-glitch <vasquezrusbel99@icloud.com>
2026-05-13 22:13:36 +0200

    Update and rename server.js to app.js

--- app.js
+++ app.js
@@ -2,15 +2,12 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-// Servir la carpeta public
 app.use(express.static(path.join(__dirname, 'public')));
 
-// FORZAR la carga del index.html en la raíz
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, '0.0.0.0', () => {
-    console.log('SERVIDOR ARRANCADO');
-});
+app.listen(PORT, () => {
+    console.log('Online');
