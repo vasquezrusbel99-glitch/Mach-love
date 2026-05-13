61ebc56f18e07385138af7c66f604e559ea0cff4
vasquezrusbel99-glitch <vasquezrusbel99@icloud.com>
2026-05-13 20:53:21 +0200

    Update server.js

--- server.js
+++ server.js
@@ -2,11 +2,12 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
+// Servir archivos estáticos de la carpeta public
 app.use(express.static(path.join(__dirname, 'public')));
 
-// ESTO ES LO QUE NECESITA RENDER:
+// Puerto dinámico para Render
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, '0.0.0.0', () => {
-    console.log(Servidor Mach Love listo);
+    console.log('Servidor Mach Love listo');
 });
